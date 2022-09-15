58125f3528f8e9b97b56dd82d62145c61772083f
息占博 <xizhanbo@huatu.com>
2022-09-15 16:55:00 +0800

    ttt

--- bootstrap/init.go
+++ bootstrap/init.go
@@ -1,33 +1,35 @@
 package bootstrap
 
+import "micro-gin/global"
+
 func init() {
 	// 初始化配置
-	//InitializeConfig()
-	//
-	//// 初始化日志
-	//global.App.Log = InitializeLog()
-	//
-	////初始化数据库
-	//global.App.DB = InitializeDB()
-	//defer func() {
-	//	if global.App.DB != nil {
-	//		db, _ := global.App.DB.DB()
-	//		db.Close()
-	//	}
-	//}()
-	//
-	//// 初始化验证器
-	//InitializeValidator()
-	//
-	//// 初始化Redis
-	//global.App.Redis = InitializeRedis()
-	//
-	//// 初始化文件系统
-	//InitializeStorage()
-	//
-	//// 初始化计划任务
-	//InitializeCron()
-	//
-	//// 启动服务器
-	//RunServer()
+	InitializeConfig()
+
+	// 初始化日志
+	global.App.Log = InitializeLog()
+
+	//初始化数据库
+	global.App.DB = InitializeDB()
+
+	defer func() {
+		if global.App.DB != nil {
+			db, _ := global.App.DB.DB()
+			db.Close()
+		}
+	}()
+	// 初始化验证器
+	InitializeValidator()
+
+	// 初始化Redis
+	global.App.Redis = InitializeRedis()
+
+	// 初始化文件系统
+	InitializeStorage()
+
+	// 初始化计划任务
+	InitializeCron()
+
+	// 启动服务器
+	RunServer()
 }
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	_ "micro-gin/bootstrap"
 )
 
 func main() {
-
-	fmt.Println("aaaaaaaa")
-	fmt.Println("aaaaaaaa")
 
 }
