4e7a6a788bb08f0c270a9d9959b79c35e26613c2
息占博 <xizhanbo@huatu.com>
2022-09-19 17:17:17 +0800

    fix bug

--- bootstrap/cron.go
+++ bootstrap/cron.go
@@ -2,20 +2,20 @@ package bootstrap
 
 import (
     "fmt"
-    "github.com/jassue/jassue-gin/global"
     "github.com/robfig/cron/v3"
+    "micro-gin/global"
     "time"
 )
 
 func InitializeCron() {
-    global.App.Cron = cron.New(cron.WithSeconds())
+	global.App.Cron = cron.New(cron.WithSeconds())
 
-    go func() {
-       global.App.Cron.AddFunc("0 0 2 * * *", func() {
-           fmt.Println(time.Now())
-       })
-       global.App.Cron.Start()
-       defer global.App.Cron.Stop()
-       select {}
-    }()
+	go func() {
+		global.App.Cron.AddFunc("0 0 2 * * *", func() {
+			fmt.Println(time.Now())
+		})
+		global.App.Cron.Start()
+		defer global.App.Cron.Stop()
+		select {}
+	}()
 }
--- go.mod
+++ go.mod
@@ -19,7 +19,8 @@ require (
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gorm.io/driver/mysql v1.3.6
-	gorm.io/gorm v1.23.8
+	gorm.io/gorm v1.23.9
+	gorm.io/plugin/dbresolver v1.2.3
 )
 
 require (
